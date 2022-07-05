573613992439a86177c7398e2f308ba902ebffbe
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-07-05 12:12:55 +0500

    Add set up

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function TodoItem(props) {
+  return <li><input
+  type="checkbox"
+  checked={this.props.todo.completed}
+  onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+/></li>
+}
+
+export default TodoItem
